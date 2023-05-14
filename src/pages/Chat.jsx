abf260b75b5418cbf75af551b0eec093726ac0d0
ariful islam <arifulislam321054@gmail.com>
2023-05-14 18:08:39 +0600

    left-sidebar chat app design1

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { CiMenuKebab } from "react-icons/ci";
 
 const Chat = () => {
   return (
-    <div className='bg-cyan-600 h-[100vh] w-full '>Chat</div>
-  )
-}
+    <div className="h-[100vh] w-full px-3">
+      <div className="grid grid-cols-12 max-w-[1600px] w-full mx-auto">
+        <div className="col-span-3">
+          <div className="h-screen w-full bg-[#0000002e] py-3">
+            <div className="bg-[#202C33] py-2 px-8 flex justify-around items-center">
+              <img
+                src={require("../img/avatar.png")}
+                alt=""
+                className="w-[40px]"
+              />
+              <button className="text-white text-xl">
+                <CiMenuKebab />
+              </button>
+            </div>
 
-export default Chat
+            <div className="my-2">
+              <input
+                type="text"
+                placeholder="Type here"
+                className="w-full max-w-xs font-semibold text-sm bg-[#202C33] text-white px-3 py-2 rounded focus:outline-slate-700 focus:border-0"
+              />
+            </div>
+
+            <div className="mt-8">
+              <div className="grid grid-cols-12 px-2">
+                <img
+                  src={require("../img/avatar.png")}
+                  alt=""
+                  className="w-[40px] col-span-2"
+                />
+                <div className="col-span-10 flex justify-between items-center gap-2 border-b border-slate-800">
+                  <div className="text-start">
+                    <h5>Shamim Hossain</h5>
+                    <p className="text-sm text-slate-500">
+                      Lorem ipsum dolor sit amet.
+                    </p>
+                  </div>
+                  <p className="text-sm text-slate-500">3:10 PM</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
