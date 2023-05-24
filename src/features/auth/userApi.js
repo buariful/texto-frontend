c72c9d526d77a263397199a2290c928503168120
ariful islam <arifulislam321054@gmail.com>
2023-05-24 16:33:40 +0600

    chatLftBar.jsx problem fix

---
+++ src/features/auth/userApi.js
@@ -0,0 +1,56 @@
+import { apiSlice } from "../api/apiSlice";
+
+export const userApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    signUp: builder.mutation({
+      query: (userData) => ({
+        url: "/v1/register",
+        method: "POST",
+        body: userData,
+        headers: {
+          "Content-Type": "application/json",
+          // Authorization: "Bearer <your_token>",
+        },
+      }),
+    }),
+    login: builder.mutation({
+      query: (userData) => ({
+        url: "/v1/login",
+        method: "POST",
+        body: userData,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+    }),
+    getUsers: builder.mutation({
+      query: ({ search, token }) => ({
+        url: `/v1/users?search=${search}`,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          // Authorization: `Bearer ${token}`,
+          Authorization: token,
+        },
+      }),
+    }),
+
+    getUserByToken: builder.mutation({
+      query: (token) => ({
+        url: `/v1/getuser`,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      }),
+    }),
+  }),
+});
+
+export const {
+  useSignUpMutation,
+  useLoginMutation,
+  useGetUsersMutation,
+  useGetUserByTokenMutation,
+} = userApi;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,131 +1,212 @@
-import React, { useState } from "react";
-import { useSignUpMutation } from "../features/userApi";
+import React, { useRef, useState, useEffect } from "react";
+import {
+  useGetUserByTokenMutation,
+  useLoginMutation,
+  useSignUpMutation,
+} from "../features/auth/userApi";
+import { useNavigate } from "react-router-dom";
+import { setUser } from "../features/auth/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
-  const [signUp, { isLoading: signUpLoading, isError: signUpError, error }] =
-    useSignUpMutation();
+  const navigate = useNavigate();
+  const [getUserByToken] = useGetUserByTokenMutation();
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.user);
+  const token = JSON.parse(localStorage.getItem("auth"));
+
+  // =============signUp and login form states =========
+  const [login] = useLoginMutation();
+  const [loginError, setLoginError] = useState("");
+
+  const [signUp] = useSignUpMutation();
+  const [signError, setSignError] = useState("");
   const [signUpData, setSignUpData] = useState({
     name: "",
     email: "",
     password: "",
   });
-  const [tab, setTab] = useState("login");
-  const [actionError, setActionError] = useState("");
+  // ========== for form tab activity =========
+  const loginBtn = useRef(null);
+  const regBtn = useRef(null);
+  const switcherTab = useRef(null);
+  const loginTab = useRef(null);
+  const registerTab = useRef(null);
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    console.log(e.target.password.value);
-  };
+  const switchTab = (string, btn1, btn2, tab1, tab2) => {
+    btn1.current.classList.add("bg-[#080d14]");
+    btn2.current.classList.remove("bg-[#080d14]");
 
-  const handleRegister = async (e) => {
-    e.preventDefault();
-    let data = await signUp(signUpData);
-    // console.log(data);
-    if (error) {
-      // setSignUpData({
-      //   name: "",
-      //   email: "",
-      //   password: "",
-      // });
-      console.log("kldsjflksd");
+    tab1.current.classList.add("block");
+    tab1.current.classList.remove("hidden");
+
+    tab2.current.classList.add("hidden");
+    tab2.current.classList.remove("block");
+
+    if (string === "login") {
+      switcherTab.current.classList.remove("translate-x-full");
+    } else {
+      switcherTab.current.classList.add("translate-x-full");
     }
   };
 
-  const loginForm = (
-    <div>
-      <form onSubmit={handleLogin}>
-        <input
-          required
-          type="email"
-          placeholder="email"
-          className="input input-bordered input-secondary w-full max-w-xs mb-2 mt-2"
-        />
+  useEffect(() => {
+    if (token) {
+      getUserByToken(token)
+        .unwrap()
+        .then((res) => {
+          dispatch(setUser(res));
+        })
+        .then((res) => navigate("/chat"))
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [dispatch, getUserByToken, token]);
 
-        <input
-          required
-          type="password"
-          placeholder="password"
-          name="password"
-          className="input input-bordered input-secondary w-full max-w-xs mb-2 "
-        />
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setLoginError("");
 
-        <button type="submit" className="btn block w-full max-w-xs m-auto">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+    const data = {
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
 
-  const registerForm = (
-    <div>
-      <form onSubmit={handleRegister}>
-        <input
-          required
-          type="text"
-          name="name"
-          placeholder="name"
-          onChange={(e) =>
-            setSignUpData({ ...signUpData, name: e.target.value })
-          }
-          value={signUpData.name}
-          className="input input-bordered input-secondary w-full max-w-xs mb-2 mt-2"
-        />
+    login(data)
+      .unwrap()
+      .then((res) => {
+        dispatch(setUser(res));
+        console.log(res);
+        localStorage.setItem("auth", JSON.stringify(res?.token));
+        navigate("/chat");
+      })
+      .catch((err) => {
+        setLoginError(err?.data?.message);
+        console.log(err);
+      });
+  };
 
-        <input
-          required
-          type="email"
-          name="email"
-          placeholder="email"
-          onChange={(e) =>
-            setSignUpData({ ...signUpData, email: e.target.value })
-          }
-          value={signUpData.email}
-          className="input input-bordered input-secondary w-full max-w-xs mb-2 mt-2"
-        />
+  const handleRegister = (e) => {
+    e.preventDefault();
+    setSignError("");
 
-        <input
-          required
-          type="password"
-          name="password"
-          placeholder="password"
-          onChange={(e) =>
-            setSignUpData({ ...signUpData, password: e.target.value })
-          }
-          value={signUpData.password}
-          className="input input-bordered input-secondary w-full max-w-xs mb-2 "
-        />
-
-        <button
-          type="submit"
-          className={`btn block w-full max-w-xs m-auto ${
-            signUpLoading && "loading "
-          }`}
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+    signUp(signUpData)
+      .unwrap()
+      .then((res) => {
+        localStorage.setItem("auth", JSON.stringify(res?.token));
+        dispatch(setUser(res));
+        navigate("/chat");
+      })
+      .catch((err) => {
+        setSignError(err?.data?.message);
+      });
+  };
 
   return (
     <div className="h-screen w-full flex items-center justify-center">
-      <div className="max-w-[500px] w-[500px] mx-auto bg-[#f3f3f4] p-5 rounded">
-        <div className="flex items-center justify-center gap-5 border-b pb-3">
+      <div className="max-w-[500px] w-[500px] mx-auto bg-gray-800 p-5 rounded overflow-hidden">
+        {/* ======== log-sign button ======= */}
+        <div className="w-full grid grid-cols-2 place-items-center">
           <button
-            className={`btn ${tab === "login" ? "bg-red-400" : "bg-black"}`}
-            onClick={() => setTab("login")}
+            ref={loginBtn}
+            onClick={() =>
+              switchTab("login", loginBtn, regBtn, loginTab, registerTab)
+            }
+            className="basis-1/2 font-semibold text-[#d6d6d6] hover:text-white  py-2 w-full text-center cursor-pointer bg-[#080d14]"
           >
-            login
+            Login
           </button>
           <button
-            className={`btn ${tab === "register" ? "bg-red-400" : "bg-black"}`}
-            onClick={() => setTab("register")}
+            ref={regBtn}
+            onClick={() =>
+              switchTab("register", regBtn, loginBtn, registerTab, loginTab)
+            }
+            className="basis-1/2 font-semibold text-[#d6d6d6] hover:text-white  py-2 w-full text-center cursor-pointer "
           >
             Register
           </button>
         </div>
-        {actionError && <p className="text-red my-2">{actionError}</p>}
-        {tab === "login" ? loginForm : registerForm}
+        <div
+          ref={switcherTab}
+          className="w-1/2 h-[2px] bg-white transition-all duration-300 ease-out"
+        ></div>
+
+        {/*======== login form =========*/}
+        <form
+          className="p-7 block text-black"
+          ref={loginTab}
+          onSubmit={handleLogin}
+        >
+          {loginError && <p className="text-red-500 my-2">{loginError}</p>}
+          <input
+            required
+            type="email"
+            placeholder="email"
+            name="email"
+            className="input input-bordered input-secondary w-full max-w-xs mb-2 mt-2"
+          />
+
+          <input
+            required
+            type="password"
+            placeholder="password"
+            name="password"
+            className="input input-bordered input-secondary w-full max-w-xs mb-2 "
+          />
+
+          <button type="submit" className="btn block w-full max-w-xs m-auto">
+            Submit
+          </button>
+        </form>
+
+        {/* ========== Register form ============ */}
+        <form
+          onSubmit={handleRegister}
+          ref={registerTab}
+          className="text-black hidden mt-5"
+        >
+          {signError && <p className="text-red-500 my-2">{signError}</p>}
+          <input
+            required
+            type="text"
+            name="name"
+            placeholder="name"
+            onChange={(e) =>
+              setSignUpData({ ...signUpData, name: e.target.value })
+            }
+            value={signUpData.name}
+            className="input input-bordered input-secondary w-full max-w-xs mb-2 mt-2 "
+          />
+
+          <input
+            required
+            type="email"
+            name="email"
+            placeholder="email"
+            onChange={(e) =>
+              setSignUpData({ ...signUpData, email: e.target.value })
+            }
+            value={signUpData.email}
+            className="input input-bordered input-secondary w-full max-w-xs mb-2"
+          />
+
+          <input
+            required
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={(e) =>
+              setSignUpData({ ...signUpData, password: e.target.value })
+            }
+            value={signUpData.password}
+            className="input input-bordered input-secondary w-full max-w-xs mb-2 "
+          />
+
+          <button type="submit" className={`btn block w-full max-w-xs m-auto`}>
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
