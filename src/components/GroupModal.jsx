77ebeec2ee2768116cf1dabc731e40aa0e163e43
ariful <arifulislam321054@gmail.com>
2023-05-28 21:39:33 +0600

    modal design update

--- src/components/GroupModal.jsx
+++ src/components/GroupModal.jsx
@@ -33,13 +33,106 @@ const GroupModal = () => {
           <h3 className="font-semibold text-sm mb-3">
             Search and select your friends
           </h3>
+          {selectedFriends}
           <input
             type="text"
             placeholder="Type here"
             className="w-full max-w-xs font-semibold text-sm bg-gray-600 text-white px-3 py-2 rounded focus:outline-slate-700 focus:border-0 mb-1"
             // onChange={handleSearch}
           />
-          {selectedFriends}
+
+          <div className="bg-slate-700 p-2 mt-2">
+            <div className="w-52 max-h-52 mx-auto overflow-auto">
+              <div className="mb-2 flex items-center gap-2 py-1 px-2 rounded hover:bg-gray-900 cursor-pointer">
+                <img
+                  src="https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1"
+                  alt=""
+                  className="w-[35px] col-span-2 rounded-full"
+                />
+                <p>name</p>
+              </div>
+              <div className="mb-2 flex items-center gap-2 py-1 px-2 rounded hover:bg-gray-900 cursor-pointer">
+                <img
+                  src="https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1"
+                  alt=""
+                  className="w-[35px] col-span-2 rounded-full"
+                />
+                <p>name</p>
+              </div>
+              <div className="mb-2 flex items-center gap-2 py-1 px-2 rounded hover:bg-gray-900 cursor-pointer">
+                <img
+                  src="https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1"
+                  alt=""
+                  className="w-[35px] col-span-2 rounded-full"
+                />
+                <p>name</p>
+              </div>
+              <div className="mb-2 flex items-center gap-2 py-1 px-2 rounded hover:bg-gray-900 cursor-pointer">
+                <img
+                  src="https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1"
+                  alt=""
+                  className="w-[35px] col-span-2 rounded-full"
+                />
+                <p>name</p>
+              </div>
+              <div className="mb-2 flex items-center gap-2 py-1 px-2 rounded hover:bg-gray-900 cursor-pointer">
+                <img
+                  src="https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1"
+                  alt=""
+                  className="w-[35px] col-span-2 rounded-full"
+                />
+                <p>name</p>
+              </div>
+              <div className="mb-2 flex items-center gap-2 py-1 px-2 rounded hover:bg-gray-900 cursor-pointer">
+                <img
+                  src="https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1"
+                  alt=""
+                  className="w-[35px] col-span-2 rounded-full"
+                />
+                <p>name</p>
+              </div>
+              <div className="mb-2 flex items-center gap-2 py-1 px-2 rounded hover:bg-gray-900 cursor-pointer">
+                <img
+                  src="https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1"
+                  alt=""
+                  className="w-[35px] col-span-2 rounded-full"
+                />
+                <p>name</p>
+              </div>
+              <div className="mb-2 flex items-center gap-2 py-1 px-2 rounded hover:bg-gray-900 cursor-pointer">
+                <img
+                  src="https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1"
+                  alt=""
+                  className="w-[35px] col-span-2 rounded-full"
+                />
+                <p>name</p>
+              </div>
+              <div className="mb-2 flex items-center gap-2 py-1 px-2 rounded hover:bg-gray-900 cursor-pointer">
+                <img
+                  src="https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1"
+                  alt=""
+                  className="w-[35px] col-span-2 rounded-full"
+                />
+                <p>name</p>
+              </div>
+              <div className="mb-2 flex items-center gap-2 py-1 px-2 rounded hover:bg-gray-900 cursor-pointer">
+                <img
+                  src="https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1"
+                  alt=""
+                  className="w-[35px] col-span-2 rounded-full"
+                />
+                <p>name</p>
+              </div>
+              <div className="mb-2 flex items-center gap-2 py-1 px-2 rounded hover:bg-gray-900 cursor-pointer">
+                <img
+                  src="https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=1"
+                  alt=""
+                  className="w-[35px] col-span-2 rounded-full"
+                />
+                <p>name</p>
+              </div>
+            </div>
+          </div>
         </div>
       </label>
     </>
