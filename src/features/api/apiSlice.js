e9071050a810c4790a878fbff0fc7b60a92bd177
ariful <arifulislam321054@gmail.com>
2023-05-30 23:07:03 +0600

    rtk query base url change

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_URL,
+    baseUrl: "https://texto-backend.onrender.com/api",
 
     prepareHeaders: (headers, { getState }) => {
       headers.set("Accept", "application/json");
