b40baab9b217ced4ae567980f6bc747fe243b344
ariful <arifulislam321054@gmail.com>
2023-06-10 21:04:46 +0600

    bakcend url change for socket

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_SERVER_URL);
+// const socket = io(process.env.REACT_APP_SERVER_URL);
+
+const socket = io("https://texto-backend.onrender.com");
 
 export default socket;
